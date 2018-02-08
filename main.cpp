0e11fe37b04863c0154a0fda70d38e9ffae4d4ae
mnasafi <mnasafi@mail.csuchico.edu>
2018-02-07 17:38:39 -0800

    prompts user for character and evaluates wether character input is uppercase, lowercase, or not a letter at all followed by ASCII value

--- main.cpp
+++ main.cpp
@@ -1,24 +1,58 @@
-//Author:
+//Author: Mustafa Nasafi
+
+//import library
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
+    
+  //declare variables
+  char letter;
+  int AsciiValue;
 
+
+  //prompt user for kybrd character input
   cout<<"What character do you want to know about?\n";
 
+  cin >> letter;
+
+  if(letter>='A' && letter <='Z')
+  {
+  
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+  cout << letter << " is an upper case letter!\n";
+
+  }
+
+  else if(letter>='a' && letter <='z')
+  {
 
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+  cout << letter << " is a lower case letter!\n";
+
+  }
+
+  else
+  {
 
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
+  cout << letter << "?! Pssh. What are you talking about?\n";
   
-  //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  }
+    
+  //end if statement
 
+
+  // cast character letter to integer ASCII value
+  AsciiValue = (int)letter;
+
+  //no matter what they enter...
+  cout << "The ASCII value is: " << AsciiValue << endl;
+
+  //exit program
   return 0;
+  
 }
+
